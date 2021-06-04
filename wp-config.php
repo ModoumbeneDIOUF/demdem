1fb599055fc1cecf84b74dff8261fc09c067bcb1
modoumbenediouf <modoumbene.diouf@uadb.edu.sn>
2021-06-04 16:47:41 +0000

    first commiteses

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'YTL3gDW9Ow' );
+define( 'DB_NAME', 'yewou_demdem' );
 
 /** MySQL database username */
-define( 'DB_USER', 'YTL3gDW9Ow' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'OlnobbJSkC' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'remotemysql.com' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
